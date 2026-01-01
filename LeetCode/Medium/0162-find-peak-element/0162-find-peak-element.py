9ea501ba8a2e8640a9f82541db6e17a21db63ffd
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-01-01 22:10:00 +0530

    0162-find-peak-element Medium .py 01-01-2026 0 ms (100%) 17.3 MB (97.2%)

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.py
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.py
@@ -1,5 +1,12 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        largest=max(nums)
-        return nums.index(largest)
+        left=0
+        right=len(nums)-1
+        while left<right:
+            mid=(right+left)//2
+            if nums[mid]<nums[mid+1]:
+                left=mid+1
+            else:
+                right=mid
+        return left
         
