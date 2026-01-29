2c79ad0aef979ffd06e0ac17c54bb3e20820d0c6
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-01-29 23:49:39 +0530

    0541-reverse-string-ii Easy .js 01-29-2026 7 ms (20.93%) 59.8 MB (5.46%)

---
+++ LeetCode/Easy/0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    let arr=s.split("")
+    for(let i=0;i<s.length;i+=2*k){
+        let l=i
+        let r= Math.min(i+k-1,arr.length-1)
+        while(l<r){
+            [arr[l],arr[r]]=[arr[r],arr[l]]
+            l++
+            r--
+        }
+    }
+    return arr.join('')
+};
