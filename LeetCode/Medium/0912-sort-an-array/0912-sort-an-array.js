104f3f4a1bc634599e2a1fde3872aa42060ba529
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-01-29 14:37:42 +0530

    0912-sort-an-array Medium .js 01-29-2026 71 ms (75.26%) 75.4 MB (81.42%)

---
+++ LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function merge(arr,left,mid,right){
+    let n1 = mid-left +1
+    let n2 = right-mid
+
+    let L = new Array(n1)
+    let R = new Array(n2)
+
+    for(let i=0;i<n1;i++){
+        L[i]=arr[left+i]
+    }
+    for(let j=0;j<n2;j++){
+        R[j]=arr[mid+1+j]
+    }
+    let i=0, j=0,k=left
+
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k]=L[i]
+            i++
+        }
+        else{
+            arr[k]=R[j]
+            j++
+        }
+        k++
+    }
+
+    while(i<n1){
+        arr[k]=L[i]
+        i++
+        k++
+    }
+    while(j<n2){
+        arr[k]=R[j]
+        j++
+        k++
+    }
+
+}
+function mergeSort(arr,left,right){
+    if(left>=right){
+        return
+    }
+    let mid = Math.floor((left+right)/2)
+    mergeSort(arr,left,mid)
+    mergeSort(arr,mid+1,right)
+    merge(arr,left,mid,right)
+}
+var sortArray = function(nums) {
+    mergeSort(nums,0,nums.length-1)
+    return nums
+};
