ae1dfa84a4a91c6fe93b2d2a89e3a500950b1310
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-01-30 10:53:35 +0530

    0205-isomorphic-strings Easy .js 01-30-2026 0 ms (100%) 55.6 MB (39.93%)

---
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let map1={}
+    let map2={}
+    for(let i=0;i<s.length;i++){
+        if(!map1[s[i]] && !map2[t[i]]){
+            map1[s[i]]=t[i]
+            map2[t[i]]=s[i]
+        }
+        else if(map1[s[i]]!==t[i] && map2[t[i]]!==s[i]) return false
+    }
+    return true
+};
