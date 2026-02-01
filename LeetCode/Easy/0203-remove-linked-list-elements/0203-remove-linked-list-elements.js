b357eb7fd2af578de23ece451b4a83773693b494
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-01 18:33:17 +0530

    0203-remove-linked-list-elements Easy .js 02-01-2026 2 ms (19.19%) 61.9 MB (26.87%)

---
+++ LeetCode/Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    let sentinel=new ListNode(0,head)
+    let curr = sentinel
+    while(curr && curr.next){
+        if(curr.next.val===val) curr.next=curr.next.next
+        else curr=curr.next
+    }
+    return sentinel.next
+};
