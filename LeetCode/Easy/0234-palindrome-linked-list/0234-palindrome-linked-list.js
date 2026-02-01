379fb413441a48867e419683092f83e379677109
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-01 17:01:22 +0530

    0234-palindrome-linked-list Easy .js 02-01-2026 16 ms (24.74%) 85.3 MB (15.16%)

---
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let arr=[]
+    let curr=head
+    while(curr){
+        arr.push(curr.val)
+        curr=curr.next
+    }
+    let l=0
+    let r=arr.length-1
+    while(l<r){
+        if(arr[l]!==arr[r]) return false
+        l++
+        r--
+    }
+    return true
+};
