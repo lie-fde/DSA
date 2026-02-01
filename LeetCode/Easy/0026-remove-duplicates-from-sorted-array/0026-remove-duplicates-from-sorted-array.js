e6e1450f81659fd4978e523861fba6275c606e64
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-01 21:49:30 +0530

    0026-remove-duplicates-from-sorted-array Easy .js 02-01-2026 0 ms (100%) 58.2 MB (46.78%)

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,11 +4,13 @@
  */
 var removeDuplicates = function(nums) {
     let l=0
-    for(let r=0;r<nums.length;r++){
-        if(nums[l]!=nums[r]){
+    let r=0
+    while(r<=nums.length){
+        if(nums[l]!==nums[r]){
             l++
             nums[l]=nums[r]
         }
+        r++
     }
-    return l+1
+    return l
 };
