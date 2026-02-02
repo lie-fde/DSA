bb37280cbd9f1a32274e9ef86f1284e3d1abd174
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-02 10:01:21 +0530

    0002-add-two-numbers Medium .js 02-02-2026 5 ms (20.57%) 62 MB (30.33%)

---
+++ LeetCode/Medium/0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let sentinel = new ListNode(0)
+    let curr = sentinel
+    let carry=0
+    while(l1 || l2 || carry){
+        let sum = (l1?.val||0) +(l2?.val||0)+carry
+        carry= Math.floor(sum/10)
+        curr.next=new ListNode(sum%10)
+        l1=l1?.next
+        l2=l2?.next
+        curr=curr.next
+    }
+    return sentinel.next
+};
