2a7f63f2da9c736bb80261dc2643c3e3ab3f31c9
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-02 14:06:10 +0530

    0415-add-strings Easy .js 02-02-2026 1 ms (84.89%) 54.1 MB (92.72%)

---
+++ LeetCode/Easy/0415-add-strings/0415-add-strings.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let n1=BigInt(num1)
+    let n2=BigInt(num2)
+    return String(n1+n2)
+};
