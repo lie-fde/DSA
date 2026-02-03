5f348699b8fc71efe6a724e5d665f60e3fba316c
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-03 09:53:25 +0530

    0278-first-bad-version Easy .js 02-03-2026 35 ms (90.16%) 53.5 MB (67.37%)

---
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l=1
+        let r=n
+        while(l<r){
+            mid=Math.floor((l+r)/2)
+            let res=isBadVersion(mid)
+            if(!res) l=mid+1
+            else r=mid 
+        }
+        return r
+    };
+};
