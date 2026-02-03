a7da337f63bd1670d46ea49431dabf9c7beed741
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-03 15:36:12 +0530

    0142-linked-list-cycle-ii Medium .js 02-03-2026 53 ms (37.11%) 57 MB (75.64%)

---
+++ LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let seen = new Set()
+    curr=head
+    while(curr){
+        if(seen.has(curr)){
+            return curr
+        }
+        seen.add(curr)
+        curr=curr.next
+    }
+    return null
+};
