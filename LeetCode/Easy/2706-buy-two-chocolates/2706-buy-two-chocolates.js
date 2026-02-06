07a505b4e3d710e712a7be7c703cd668e63c9e3f
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-06 11:14:03 +0530

    2706-buy-two-chocolates Easy .js 02-06-2026 3 ms (50%) 59.1 MB (36.43%)

---
+++ LeetCode/Easy/2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function (prices, money) {
+   prices.sort((a,b)=>a-b)
+   let sum=prices[0]+prices[1]
+   if(sum<=money) return money-sum
+   return money
+};
