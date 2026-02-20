819f94b8f4be4edb32fe586a424a06401754528f
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-02-20 10:47:41 +0530

    0098-validate-binary-search-tree Medium .js 02-20-2026 0 ms (100%) 59.2 MB (55.39%)

---
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    function valid(node,left,right){
+        if(!node) return true
+        if(!(node.val<right && node.val >left)){
+            return false
+        }
+        return (valid(node.left,left,node.val) && valid(node.right,node.val,right))
+    }
+    return valid(root,-Infinity,Infinity)
+};
