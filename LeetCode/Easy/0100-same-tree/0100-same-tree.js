7f792eb5b9286bcb76fa140613f5c1b30ee94923
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-03-05 08:31:35 +0530

    0100-same-tree Easy .js 03-05-2026 0 ms (100%) 52.9 MB (96.55%)

--- LeetCode/Easy/0100-same-tree/0100-same-tree.js
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.js
@@ -18,5 +18,5 @@ var isSameTree = function(p, q) {
 
      if(p.val != q.val) return false
 
-     return (isSameTree(p.left,q.left) || isSameTree(p.right,q.right))
+     return (isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
 };
