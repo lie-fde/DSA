5a30e7805da416b44189271bad53bbe738e7ca6d
SHIBIN M S <112691357+lie-fde@users.noreply.github.com>
2026-03-12 23:12:41 +0530

    0917-reverse-only-letters Easy .js 03-12-2026 N/A (0%) N/A (0%)

--- LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.js
+++ LeetCode/Easy/0917-reverse-only-letters/0917-reverse-only-letters.js
@@ -2,23 +2,22 @@
  * @param {string} s
  * @return {string}
  */
-var reverseOnlyLetters = function(s) {
+var reverseOnlyLetters = function (s) {
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    let l=0
-    let r=s.length-1
+    let l = 0
+    let r = s.length - 1
     let word = s.split('')
-    while(l<r){
-       while(!letters.includes(s[l]) && l<s.length-1){
-        l++
-       }
-       console.log("l",l)
-       while(!letters.includes(s[r]) && r>0){
-        r--
-       }
-       
-    if (l<r) [word[l],word[r]] = [word[r],word[l]] 
-       l++;
-       r--;
+    while (l < r) {
+        while (!letters.includes(word[l]) && l < r) {
+            l++
+        }
+        while (!letters.includes(word[r]) && r > l) {
+            r--
+        }
+
+        if (l < r) [word[l], word[r]] = [word[r], word[l]]
+        l++;
+        r--;
     }
 
     return word.join("")
